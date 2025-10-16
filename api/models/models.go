26637eb7d19dc916450aa5dde424b8104a307916
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-10-16 22:24:11 +0100

    add user and profile response models

---
+++ api/models/models.go
@@ -0,0 +1,14 @@
+package models
+
+type User struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+	Stack string `json:"stack"`
+}
+
+type ProfileResponse struct {
+	Status    string `json:"status"`
+	User      User   `json:"user"`
+	Timestamp string `json:"timestamp"`
+	Fact      string `json:"fact"`
+}
